5837377edf18eb5f5c8571409480619d2e59af3e
rLuann <rluann13@gmail.com>
2026-07-12 15:14:06 -0300

    Adicionando classe linha à refatoração poo

---
+++ poo/linhas.py
@@ -0,0 +1,21 @@
+from figuras import *
+
+# criando subclasse linha
+class Linha(Figura):
+    # aqui utilizamos os kwargs (keyword arguments) para que ela guardasse a cor_preenchimento, já que a linha não tem. Porque a função do main passa cor_preenchimento para todas as subclasses.
+    def __init__(self, x1, y1, cor_borda = "black", **kwargs):
+        super().__init__(cor_borda = cor_borda, cor_preenchimento = None)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x1  
+        self.y2 = y1
+    
+    def atualizar(self, event):
+        self.x2 = event.x
+        self.y2 = event.y
+    
+    def desenhar(self, canvas, dash=None):
+        canvas.create_line( self.x1, self.y1, self.x2, self.y2, fill = self.cor_borda, dash = dash)
+    
+    def esta_incompleta(self):
+        return (self.x1, self.y1) == (self.x2, self.y2)
